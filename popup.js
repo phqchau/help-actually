8c017e8b7e7b315520e35a89aa6e31cc72fb1f83
FLANNERY-LAPTOP\Flannery <flancor@swbell.net>
2016-10-08 22:56:57 -0400

    Output rating

--- popup.js
+++ popup.js
@@ -14,22 +14,22 @@ function getCurrentTabUrl(callback) {
 }
 
 var charities = {
-  'redcross.org' : 'The American Red Cross',
+  'redcross.org' : ['The American Red Cross', '3', 'Doctors Without Borders',  'All Hands Volunteers', ' Haiti Communitiere'],
   'autismspeaks.org' : 'Autism Speaks',
   'komen.org' : 'Susan G Komen for the Cure',
   'kidswishnetwork.org' : 'Kids Wish Network',
   'nvsf.org' : 'National Veterans Services Fund',
   'childrenscharityfund.org' : 'Children\'s Charity Fund Inc.',
-  'ffcf.org' : 'Firefighter\'s Charitable Foundation'
+  'ffcf.org' : 'Firefighter\'s Charitable Foundation',
   'findthekids.org' : 'The Committee for Missing Children',
   'cancersurvivorsfund.org' : 'Cancer Survivors\' Fund',
-  '.spcai.org' : 'SPCA International',
-  '.peta.org' : 'People for the Ethical Treatment of Animals (PETA)',
-  '.clintonfoundation.org' : 'Clinton Foundation',
-  '.salvationarmyusa.org' : 'The Salvation Army',
-  '.pva.org' : 'Paralyzed Veterans of America',
-  '.diabetes.org' : 'American Diabetes Association',
-  '.worldwildlife.org' : 'World Wildlife Fund'
+  'spcai.org' : 'SPCA International',
+  'peta.org' : 'People for the Ethical Treatment of Animals (PETA)',
+  'clintonfoundation.org' : 'Clinton Foundation',
+  'salvationarmyusa.org' : 'The Salvation Army',
+  'pva.org' : 'Paralyzed Veterans of America',
+  'diabetes.org' : 'American Diabetes Association',
+  'worldwildlife.org' : 'World Wildlife Fund'
 }
 
 function renderStatus(statusText) {
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     for (var x in charities) {
       if (url.indexOf(x) !== -1) {
-        renderStatus('Organization name is ' + charities[x]);
+        renderStatus('Organization name:\n' + charities[x][0] + '\nCharity Navigator Ranking:\n' + charities[x][1]);
         break;
       }
       renderStatus('This charity is not in our database!');
