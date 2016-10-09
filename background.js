4d2b75c75e8bac3efcc5d0fa33e8b8610cb87cda
FLANNERY-LAPTOP\Flannery <flancor@swbell.net>
2016-10-08 22:03:20 -0400

    all links

--- background.js
+++ background.js
@@ -6,8 +6,51 @@ chrome.runtime.onInstalled.addListener(function(details) {
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
     if (tab.url.toLowerCase().indexOf("autismspeaks.org") > -1){
         chrome.pageAction.show(tab.id);
+		chrome.tabs.executeScript(null, {"file": "popup.html"});
     }
-	if (tab.url.toLowerCase().indexOf("redcross.org") > -1){
+	else if (tab.url.toLowerCase().indexOf("redcross.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("komen.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("kidswishnetwork.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("nvsf.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("childrenscharityfund.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("ffcf.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("findthekids.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("cancersurvivorsfund.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("spcai.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("peta.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("clintonfoundation.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("salvationarmyusa.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("pva.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("diabetes.org") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+	else if (tab.url.toLowerCase().indexOf("worldwildlife.org") > -1){
         chrome.pageAction.show(tab.id);
     }
 });
